73b95cf7067ebdc99594a9ecf3d780aa9cc59155
csbkwang <csbkwang@gmail.com>
2017-10-21 00:20:54 +0800

    Add files via upload

---
+++ train_adv_crossmodal_triplet_wiki.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+from models.adv_crossmodal_triplet_wiki import AdvCrossModalSimple, ModelParams
+#from models.wiki_shallow import AdvCrossModalSimple, ModelParams
+def main(_):
+    graph = tf.Graph()
+    model_params = ModelParams()
+    model_params.update()
+
+    with graph.as_default():
+        model = AdvCrossModalSimple(model_params)
+    with tf.Session(graph=graph) as sess:
+        model.train(sess)
+        #model.eval_random_rank()
+        model.eval(sess)
+
+
+if __name__ == '__main__':
+    tf.app.run()
